b7c9e18b8ebee4a27099838c2606b3a42772ae99
Sayantan Biswas <61279052+sayantan02@users.noreply.github.com>
2023-02-19 17:22:57 +0530

    Update constants.js

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -4,10 +4,3 @@ export const contractABI = abi.abi;
 
 // For remote chain.
 export const contractAddress = "0x16Cc6dceb3496Bc2d76e984c53DCD6Bc6d7fD9fd";
-// export const contractAddress = "0xafE152c3849F3bC220aC025f694622D9014adcb3";
-
-// NFT local chain.
-// export const contractAddress = "0x458af44a0b5f20D5E85e322d49d17f51b2466B8a";
-
-// For localhost chain.
-// export const contractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
